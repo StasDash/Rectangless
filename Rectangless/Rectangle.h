51cd3d6c80789c87c9099af6d64d7cc6cadef40a
StasDash <stanislav.daschkevitch@yandex.by>
2020-12-01 09:59:34 +0000

    Commit from GitHub Actions

--- Rectangless/Rectangle.h
+++ Rectangless/Rectangle.h
@@ -1,5 +1,6 @@
-// This is an open source non-commercial project. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+// This is an open source non-commercial project. Dear PVS-Studio, please check
+// it. PVS-Studio Static Code Analyzer for C, C++, C#, and Java:
+// http://www.viva64.com
 #ifndef FIGURES_RECTANGLE_H
 #define FIGURES_RECTANGLE_H
 
