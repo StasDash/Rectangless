6c2c1cf4ba6dddebd13ed263b2cf8ec28646c75d
StasDash <stanislav.daschkevitch@yandex.by>
2020-12-01 09:21:47 +0000

    Commit from GitHub Actions

--- Rectangless/option.h
+++ Rectangless/option.h
@@ -1,16 +1,14 @@
 #pragma once
 
 namespace figures {
-	template<class T>
-	class Option {
-	private:
-		T obj;
-		bool isValue;
-	public:
-		Option() : obj(nullptr), isValue(false) {}
-		Option(T& element) : obj(element), isValue(true) {}
-		T& unwrap() {
-			return obj;
-		}
-	};
-}
+template <class T> class Option {
+private:
+  T obj;
+  bool isValue;
+
+public:
+  Option() : obj(nullptr), isValue(false) {}
+  Option(T &element) : obj(element), isValue(true) {}
+  T &unwrap() { return obj; }
+};
+} // namespace figures
